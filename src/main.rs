3d2b880f76d7b29ce0396254d5a5b0105380d70c
Jakub Beránek <berykubik@gmail.com>
2021-06-22 15:48:44 +0200

    Add ureq

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct S {
+    a: u32
+}
+
 fn main() {
+    let s: S = serde_json::from_slice(&[]).unwrap();
     ureq::get("http://seznam.cz");
     println!("hello");
 }
